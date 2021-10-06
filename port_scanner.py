5ab9c3ea7206a28920d512ceaf7fbb6edae13481
zivkovtun <39801525+zivkovtun@users.noreply.github.com>
2021-10-06 14:11:17 +0300

    Deleted unneeded lines of code.

--- port_scanner.py
+++ port_scanner.py
@@ -16,8 +16,6 @@ if correct.upper() == 'N':
 if correct.upper() == 'Y':
     # creating a file to be filled with open ports
     with open('openPort.txt', 'w') as oPf:
-        # creating a log file to be filled with errors
-        #with open('errLog.txt', 'w')  as errLog:
             # Create and configure logger
             logging.basicConfig(filename="Error.log",
                                 format='%(asctime)s %(message)s',
@@ -33,7 +31,7 @@ if correct.upper() == 'Y':
                 #getting ip address ipv4 from string that represents ip.
                 machineIp = socket.gethostbyname(ipInput)
                 #check ports 1-65535
-                for port in range(1, 65535):
+                for port in range(1, 1000):
                     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                     result = sock.connect_ex((machineIp, port))
                     if result == 0:
@@ -56,7 +54,6 @@ if correct.upper() == 'Y':
 
 
             except socket.error as er:
-                errLog.write("Error number: [%s]  %s\n" % (er.errno, er.strerror))
                 sys.stdout.write("Error number: [%s] %s\n" % (er.errno, er.strerror))
                 #logger Test messages
                 elog.error("Error number: [%s] %s\n" % (er.errno, er.strerror))
